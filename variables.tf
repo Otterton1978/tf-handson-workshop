c7615d4f86b547da36d763df3c40c1c48f4b2601
Otterton1978 <yohei@hashicorp.com>
2020-04-17 16:09:12 +1000

    modify variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "access_key" {
+    type = string
+    description = "The AWS access key"
+}
+
+variable "secret_key" {
+    type = string
+    description = "The AWS secret key."
+}
 
 variable "region" {
     type = string
